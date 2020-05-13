3649db3cec9cb5e3eed1c799ae61327594ca5f8e
qiubee <qiubees@hotmail.com>
2020-05-13 19:53:12 +0200

    add secret & home route

--- routes/router.js
+++ routes/router.js
@@ -5,12 +5,20 @@ const beginpage = require("./controllers/start");
 const loginpage = require("./controllers/login");
 const signuppage = require("./controllers/signup");
 const aboutpage = require("./controllers/about");
+const secret = require("./controllers/secret");
+const parameter = require("./controllers/parameter");
+const realSecret = require("./controllers/gifs");
+const secretDownload = require("./controllers/secretdownload");
 const error = require("./controllers/error");
 
 router.get("/", beginpage);
 router.get("/login", loginpage);
 router.get("/signup", signuppage);
 router.get("/about", aboutpage);
+router.get(/.*secret\/?$/, secret);
+router.get("/home/:user", parameter);
+router.get("/secret/:item", realSecret);
+router.get("/secret/cats?/:download", secretDownload);
 router.get("*", error); // give error on any route that has not been defined
 
 module.exports = router;
