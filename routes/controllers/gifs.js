1857782b7b865680e6b458df13d641ec6ab24ca8
qiubee <qiubees@hotmail.com>
2020-05-15 20:09:37 +0200

    add CSP

--- routes/controllers/gifs.js
+++ routes/controllers/gifs.js
@@ -1,8 +1,13 @@
 function randomCatGIF () {
 	const giphyDomain = "https://giphy.com/gifs/";
-	const cats = ["cat-hello-oh-MWSRkVoNaC30A", "jerseydemic-3oriO0OEd9QIDdllqo", "cat-sup-whats-up-onOWJOc7U5GAE", "funny-cat-mlvseq9yvZhba"];
+	const cats = ["cat-hello-oh-MWSRkVoNaC30A", "jerseydemic-3oriO0OEd9QIDdllqo", "cat-sup-whats-up-onOWJOc7U5GAE", "aww-11s7Ke7jcNxCHS", "cat-hello-hey-VOPK1BqsMEJRS", "cat-loop-running-10SqrGYtLNcNWw"];
 	const index = Math.floor(Math.random() * cats.length);
 	return giphyDomain + cats[index];
+}
+
+function generateNonce () {
+	// generate random strings: https://gist.github.com/6174/6062387
+	return Math.random().toString(31).substring(4, 14) + Math.random().toString(21).substring(2, 9);
 }
 
 function cats (req, res) {
@@ -10,15 +15,18 @@ function cats (req, res) {
 	console.log(item);
 	switch (true) {
 	case /cats?/g.test(item): {
-		const catGIF = randomCatGIF();
-		const gifID = catGIF.split("-").pop();
-		const catGIFembed = "https://giphy.com/embed/" + gifID;
+		const catGIFsrc = randomCatGIF();
+		const gifID = catGIFsrc.split("-").pop();
+		const catGIF = "https://media.giphy.com/media/" + gifID + "/source.gif";
+		const nonce = generateNonce();
+		res.setHeader("Content-Security-Policy", "script-src 'self', script-src 'nonce-" + nonce + "';");
 		return res.render("cat", {
 			layout: "dark",
 			title: "Look, here's a cat",
 			hellocat: gifID === "MWSRkVoNaC30A" ? true : false,
-			gifembed: catGIFembed,
-			gif: catGIF
+			gifsrc: catGIFsrc,
+			gif: catGIF,
+			randomString: nonce
 		});
 	}
 	default:
