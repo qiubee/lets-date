83ab89f8e88a0b0565859c8af1f47a8a2fc77945
qiubee <qiubees@hotmail.com>
2020-05-15 15:24:47 +0200

    add gif download response

--- routes/controllers/secretdownload.js
+++ routes/controllers/secretdownload.js
@@ -1,12 +1,9 @@
-const path = require("path");
-
 function downloadSecret (req, res) {
-	const item = res.params.download;
-	console.log(item);
+	const item = req.params.download;
 	if (item === "hellocat.gif") {
-		res.sendFile("8VPNV2H.gif", {
-			root: path.join(__dirname, "../public")
-		});
+		res.download("./public/images/8VPNV2H.gif", "hellocat.gif");
+	} else {
+		res.redirect("/secret/cats");
 	}
 }
 
