90f1db24571d354db2b2064cd54c7a5fe4133ad6
qiubee <qiubees@hotmail.com>
2020-05-15 20:10:08 +0200

    refactor routes

--- routes/router.js
+++ routes/router.js
@@ -11,14 +11,17 @@ const realSecret = require("./controllers/gifs");
 const secretDownload = require("./controllers/secretdownload");
 const error = require("./controllers/error");
 
-router.get("/", beginpage);
-router.get("/login", loginpage);
-router.get("/signup", signuppage);
-router.get("/about", aboutpage);
-router.get(/.*secret\/?$/, secret);
-router.get("/home/:user", parameter);
-router.get("/secret/:item", realSecret);
-router.get("/secret/cats?/:download", secretDownload);
-router.get("*", error); // give error on any route that has not been defined
+router.get("/", beginpage)
+	.get("/login", loginpage)
+	.get("/signup", signuppage)
+	.get("/about", aboutpage)
+	.get(/.*secret\/?$/, secret)
+	.get("/home/:user", parameter)
+	.get("/secret/:item", realSecret)
+	.get("/secret/cats?/:download", secretDownload)
+	.get("/report", function (req, res) {
+		res.json();
+	})
+	.get("*", error); // give error on any route that has not been defined
 
 module.exports = router;
