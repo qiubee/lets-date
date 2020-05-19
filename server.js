e64ba9d003c788ab0d62704063e8770075e39849
qiubee <qiubees@hotmail.com>
2020-05-19 09:18:13 +0200

    refactor code

--- server.js
+++ server.js
@@ -10,25 +10,18 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
-// set handlebars as templating engine
-app.set("view engine", "hbs");
-app.engine( "hbs", hbs({ 
-	extname: "hbs", 
-	defaultLayout: "default", 
-	layoutsDir: __dirname + "/views/layouts/",
-	partialsDir: __dirname + "/views/partials/"
-}));
 
-// use public folder for static files
-app.use(express.static("public"));
-
-// routing
-app.use(router);
-app.use(errorHandler);
-
-// security
-app.disable("x-powered-by");
-
-app.listen(port, function () {
-	console.log(`Listening on port \u001b[1m\u001b[36m${port}\u001b[0m\n\u001b[1m\u001b[32mlocalhost:${port}\u001b[0m`);
-});
+app.set("view engine", "hbs") // set handlebars as templating engine
+	.engine( "hbs", hbs({ 
+		extname: "hbs", 
+		defaultLayout: "default", 
+		layoutsDir: __dirname + "/views/layouts/",
+		partialsDir: __dirname + "/views/partials/"
+	}))
+	.use(express.static("public")) // use public folder for static files
+	.use(router) // routing
+	.use(errorHandler)
+	.disable("x-powered-by") // security
+	.listen(port, function () {
+		console.log(`Listening on port \u001b[1m\u001b[36m${port}\u001b[0m\n\u001b[1m\u001b[32mlocalhost:${port}\u001b[0m`);
+	});
