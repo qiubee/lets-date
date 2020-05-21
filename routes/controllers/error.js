2e7e91984a01c4c77449fc0d32b7e58a8037d15d
qiubee <qiubees@hotmail.com>
2020-05-21 18:28:24 +0200

    update error message

--- routes/controllers/error.js
+++ routes/controllers/error.js
@@ -1,5 +1,5 @@
 function error (req, res, next) {
-	const err = new Error("User tried to reach " + req.originalUrl + ": Page not found");
+	const err = new Error("404 - Page not found: User tried to reach " + req.originalUrl);
 	err.url = req.originalUrl;
 	err.status = 404;
 	err.redirect = true;
