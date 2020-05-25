f21adc6ee0a7318db4e30f649e2eb5f6307c3cc3
qiubee <qiubees@hotmail.com>
2020-05-25 15:52:59 +0200

    add database & host var

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const dotenv = require("dotenv");
 const express = require("express");
 const hbs = require("express-handlebars");
 const router = require("./routes/router");
+const database = require("./database/database");
 const errorHandler = require("./routes/errorHandler");
 
 dotenv.config();
 
 // server
 const app = express();
+const host = process.env.HOST || "localhost";
 const port = process.env.PORT || 8000;
-
 
 app.set("view engine", "hbs") // set handlebars as templating engine
 	.engine( "hbs", hbs({ 
@@ -20,9 +21,10 @@ app.set("view engine", "hbs") // set handlebars as templating engine
 	}))
 	.use(express.static("public")) // use public folder for static files
 	.use(express.urlencoded({extended: true})) // to get data from http body
+	.use(database) // database processing
 	.use(router) // routing
 	.use(errorHandler)
 	.disable("x-powered-by") // security
 	.listen(port, function () {
-		console.log(`Listening on port \u001b[1m\u001b[36m${port}\u001b[0m\n\u001b[1m\u001b[32mlocalhost:${port}\u001b[0m`);
+		console.log(`Listening on port \u001b[1m\u001b[36m${port}\u001b[0m\n\u001b[1m\u001b[32m${host}:${port}\u001b[0m`);
 	});
