a8803c6af8e4853964559a63c0b4798ab85fd496
qiubee <qiubees@hotmail.com>
2020-05-25 15:53:43 +0200

    add database processor

---
+++ database/database.js
@@ -0,0 +1,23 @@
+const mongodb = require("mongodb");
+
+// database
+const MongoClient = mongodb.MongoClient;
+const db = null || connectDB();
+
+async function connectDB () {
+	const uri = "mongo+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_HOSTNAME + "/test?retryWrites=true&w=majority";
+	const client = new MongoClient(uri);
+	try {
+		await client.connect();
+		return client.db("liev");
+	} catch (err) {
+		console.error(err);
+	} 
+	// finally {
+	// 	await client.close();
+	// }
+}
+
+// connectDB();
+
+module.exports = connectDB;
