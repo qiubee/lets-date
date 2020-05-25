c252f2d4e2790162a3d4912c5e11f0bb605b0719
qiubee <qiubees@hotmail.com>
2020-05-25 17:15:59 +0200

    fix uri

--- database/database.js
+++ database/database.js
@@ -4,7 +4,7 @@ const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
 async function connectDB () {
-	const uri = "mongo+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_HOSTNAME + "/test?retryWrites=true&w=majority";
+	const uri = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_HOSTNAME + "/test?retryWrites=true&w=majority";
 	const client = new MongoClient(uri);
 	try {
 		await client.connect();
