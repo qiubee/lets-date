84604090d6f957cfbd40aec7940fa35764aaa388
qiubee <qiubees@hotmail.com>
2020-05-30 17:39:14 +0200

    remove unused modules, update profile module

--- routes/router.js
+++ routes/router.js
@@ -3,10 +3,10 @@ const router = require("express").Router();
 // controllers
 const index = require("./controllers/index"),
 	login = require("./controllers/login"),
+	logout = require("./controllers/logout"),
 	signup = require("./controllers/signup"),
 	about = require("./controllers/about"),
 	secret = require("./controllers/secret"),
-	userHome = require("./controllers/home"),
 	mail = require("./controllers/mail"),
 	profile = require("./controllers/profile"),
 	realSecret = require("./controllers/gifs"),
@@ -17,14 +17,15 @@ const index = require("./controllers/index"),
 
 router.get("/", index)
 	.get("/login", login)
+	.get("/logout", logout)
 	.get("/signup", signup)
+	.get("/create-profile", profile.create)
 	.get("/about", about)
 	.get(/.*secret\/?$/, secret)
 	.get("/secret/:item", realSecret)
 	.get("/secret/cats?/:download", secretDownload)
 	// User pages
-	.get("/home", userHome)
-	.get("/profile/", profile)
+	.get("/profile", profile.get)
 	.get("/mail", mail)
 	// Data processing
 	.post("/login", processLogin)
