134bcb98eae069c1a1f02888855282b420ae25b3
qiubee <qiubees@hotmail.com>
2020-05-30 17:43:03 +0200

    refactor module & add authentication check

--- routes/controllers/profile.js
+++ routes/controllers/profile.js
@@ -1,15 +1,34 @@
-function profilepage (req, res) {
-	const name = "name",
-		age = 18,
-		city = "Paris",
-		country = "France";
+function get (req, res) {
+	if (!req.session.auth) {
+		res.status(401);
+		return res.redirect("/login");
+	}
 	res.render("profile", {
 		title: "Edit profile",
-		name: name,
-		age: age,
-		city: city,
-		country: country
+		// name: name,
+		// age: age,
+		// city: city,
+		// country: country
 	});
 }
 
-module.exports = profilepage;
+function setup (req, res) {
+	if (!req.session.auth) {
+		res.status(401);
+		return res.redirect("/login");
+	}
+	res.render("create-profile");
+}
+
+function create (req, res) {
+	// process new user information and add to database
+}
+
+function update (req, res) {
+	// update profile changes to database and refresh profile page
+}
+
+exports.get = get;
+exports.getSetup = setup;
+exports.create = create;
+exports.update = update;
