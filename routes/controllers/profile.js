95040dcea24e1cd946acdc9acd42636c8921af0c
qiubee <qiubees@hotmail.com>
2020-05-31 17:50:54 +0200

    update functions to connect to database

--- routes/controllers/profile.js
+++ routes/controllers/profile.js
@@ -1,14 +1,27 @@
-function get (req, res) {
+const { getAge } = require("../../modules/age");
+const { getCountryName } = require("../../modules/country");
+const db = require("../../database/database");
+
+async function get (req, res) {
 	if (!req.session.auth) {
 		res.status(401);
 		return res.redirect("/login");
 	}
+	let age, country, place;
+	await db.connect(async function(db) {
+		const user = await db.collection("users").find({ email : { $eq: req.session.email}});
+		country = getCountryName(user.country);
+		age = getAge(user.age[0], user.age[1]);
+		if (user.place) {
+			place = user.place;
+		}
+	});
 	res.render("profile", {
 		title: "Edit profile",
-		// name: name,
-		// age: age,
-		// city: city,
-		// country: country
+		name: req.session.name,
+		age: age,
+		country: country,
+		place: place
 	});
 }
 
@@ -18,6 +31,7 @@ function setup (req, res) {
 		return res.redirect("/login");
 	}
 	res.render("create-profile", {
+		title: "Set up your profile - Liev",
 		maxYear: new Date().getFullYear() - 18
 	});
 }
@@ -25,7 +39,13 @@ function setup (req, res) {
 function create (req, res) {
 	const user = req.body;
 	const name = user.name.replace(/[\d\W\0_]/g, "");
-	// process new user information and add to database
+	db.update("users", { email: { $eq: req.session.email }}, { $set: { 
+		name: name,
+		age: user.age,
+		country: user.country
+	}});
+	req.session.name = name;
+	res.redirect("/");
 }
 
 function update (req, res) {
