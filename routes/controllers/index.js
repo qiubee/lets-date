794be2f1625ec68017a9d82270823e23d3609583
qiubee <qiubees@hotmail.com>
2020-06-01 21:17:11 +0200

    add route to user page when authenticated

--- routes/controllers/index.js
+++ routes/controllers/index.js
@@ -1,7 +1,14 @@
 function start (req, res) {
-	res.render("index", {
-		title: "Liev - Let your words find love"
-	});
+	if (!req.session.auth) {
+		return res.render("index", {
+			title: "Liev - Let your words find love"
+		});
+	} else if (req.session.auth) {
+		return res.render("home", {
+			title: "Liev",
+			stamp: "images/stamps/GB-stamp.png",
+		});
+	}
 }
 
 module.exports = start;
