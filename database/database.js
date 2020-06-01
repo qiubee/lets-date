cc2801c47140378f25a5531d65ecebaffe045e58
qiubee <qiubees@hotmail.com>
2020-06-01 12:12:55 +0200

    add configuration

--- database/database.js
+++ database/database.js
@@ -1,18 +1,19 @@
 const MongoClient = require("mongodb").MongoClient;
+const { dbuser, dbpw, dbhost, dbname } = require("../config");
 
 async function connect (callback) {
-	const uri = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_HOST + "/" + process.env.DB_NAME;
+	const uri = "mongodb+srv://" + dbuser + ":" + dbpw + "@" + dbhost + "/" + dbname;
 	const client = new MongoClient(uri, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true
 	});
-	console.log("\u001b[1m\u001b[37m-- \u001b[32mConnecting to " + process.env.DB_HOST + " \u001b[37m--\u001b[0m");
+	console.log("\u001b[1m\u001b[37m-- \u001b[32mConnecting to " + dbhost + " \u001b[37m--\u001b[0m");
 	try {
 		await client.connect();
 		console.log("\u001b[1m\u001b[37m-- \u001b[32mConnected \u001b[37m--\u001b[0m");
 		console.log("\u001b[37mAccess granted\u001b[0m");
-		const db = client.db(process.env.DB_NAME);
-		console.log("\u001b[37mYou have access to database: " + process.env.DB_NAME + "\u001b[0m");
+		const db = client.db(dbname);
+		console.log("\u001b[37mYou have access to database: " + dbname + "\u001b[0m");
 		console.log("\u001b[37mPerforming action...\u001b[0m");
 		await callback(db);
 		console.log("\u001b[32mAction completed\u001b[0m");
