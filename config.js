aedb00cbb7768cd93475fd82b9d5ead53bfd40f4
qiubee <qiubees@hotmail.com>
2020-06-01 12:11:54 +0200

    add .env configuration file

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	port: process.env.PORT,
+	host: process.env.HOST,
+	dbname: process.env.DB_NAME,
+	dbuser: process.env.DB_USER,
+	dbpw: process.env.DB_PASSWORD,
+	dbhost: process.env.DB_HOST,
+	secret: process.env.S_SECRET
+};
